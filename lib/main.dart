4663c40ecb7b5abc2210fa2f8a25d9f1fa10a7f6
AzMy27 <azmi.bengkalis@gmail.com>
2023-12-05 10:46:54 +0700

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -10,18 +10,62 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(20.0), // Jawaban Soal 1: Menentukan padding
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment
+                  .start, // Jawaban Soal 2: Menentukan crossAxisAlignment
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(
+                      bottom: 8.0), // Jawaban Soal 2: Menentukan padding bottom
+                  child: const Text(
+                    'Wisata Gunung di Batu',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  'Batu, Malang, Indonesia',
+                  style: TextStyle(
+                    color: Colors
+                        .grey[500], // Jawaban Soal 2: Menentukan warna teks
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Icon(
+            Icons.star, // Jawaban Soal 3: Menentukan jenis icon
+            color: Colors.red, // Jawaban Soal 3: Menentukan warna icon
+          ),
+          const Text('41'), // Jawaban Soal 3: Menentukan teks
+        ],
+      ),
+    );
+
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Layout, Muhammad Azmi 6304211396',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        primarySwatch: Colors.deepPurple,
         useMaterial3: true,
       ),
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Flutter layout demo'),
         ),
-        body: const Center(
-          child: Text('Hello World'),
+        body: Column(
+          children: [
+            const Center(
+              child: Text('Hello World'),
+            ),
+            titleSection,
+          ],
         ),
       ),
     );
